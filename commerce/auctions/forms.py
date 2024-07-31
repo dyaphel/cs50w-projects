7b4292c2af1638510c6897739f83d14b82e5290c
Carlo <carlosegreto95@gmail.com>
2024-07-31 16:50:42 +0200

    forms updtae

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -2,13 +2,44 @@ from django import forms
 from .models import AuctionListings
 
 class AuctionListingForm(forms.ModelForm):
+    title = forms.CharField( 
+        required=True, 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter the title'
+        })
+    )
+    description = forms.TextInput( 
+        required=True, 
+        widget=forms.Textarea(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter the description',
+             'style': 'height: 12vh;'
+        })
+    )
+    
+    starting_bid = forms.NumberInput( 
+        required=True, 
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter the starting bid',
+        })
+    )
+    
+    image_url = forms.CharField( 
+        widget=forms.URLInput(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter the image url',
+        })
+    )
+
+    category = forms.Select( 
+        widget=forms.Select(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter the description',
+             'style': 'height: 12vh;'
+        })
+    )
     class Meta:
         model = AuctionListings 
         fields = ['title', 'description', 'starting_bid', 'image_url', 'category'] 
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter the title'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter the description','style': 'height: 12vh;'}),
-            'starting_bid': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter the starting bid'}),
-            'image_url': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Enter the image URL'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-        }
