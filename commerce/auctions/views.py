59c80b908c83d5aef03e4cdac788e6adb5d0e6fa
Carlo <carlosegreto95@gmail.com>
2024-08-19 23:17:22 +0200

    up

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -204,19 +204,11 @@ def categories(request):
 def category_items(request, category_name):
     category_map = dict(AuctionListing.CATEGORY_LIST)
     category_code = None
-
-    # Trova il codice della categoria in base al nome
     for code, name in category_map.items():
         if name == category_name:
             category_code = code
             break
-
-    if category_code is None:
-        return render(request, "auctions/error.html", {
-            "message": "Category not found."
-        })
-
-    # Filtra gli items per categoria e attivi
+    
     items = AuctionListing.objects.filter(category=category_code, active=True)
     
     return render(request, "auctions/category_items.html", {
