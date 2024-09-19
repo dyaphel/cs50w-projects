399a3dc34b9466ad30b60a7abd629bf8143a4fa9
Carlo <carlosegreto95@gmail.com>
2024-09-19 16:47:43 +0200

    post model up

--- network/project4/network/models.py
+++ network/project4/network/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+class Post ( models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body =models.TextField()
+    date = models.DateTimeField( default=timezone.now)
+    like = models.PositiveIntegerField()
